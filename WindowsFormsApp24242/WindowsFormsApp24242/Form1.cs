e2d63880ea8491c6f342ae0d95cb723c389062cb
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-09-28 10:39:53 +0200

    Add files via upload

--- WindowsFormsApp24242/WindowsFormsApp24242/Form1.cs
+++ WindowsFormsApp24242/WindowsFormsApp24242/Form1.cs
@@ -20,39 +20,59 @@ namespace WindowsFormsApp24242
 
         private void button1_Click(object sender, EventArgs e)
         {
+            StreamReader ctenar = null;
             try
             {
                 int n = int.Parse(textBox1.Text);
-                StreamReader ctenar = new StreamReader("cisla.txt");
+                ctenar = new StreamReader("cisla.txt");
                 int poradi = 1;
                 int[] pole = new int[100];
-                int mocnina = 0;
+                double mocnina = 0;
                 int podil_celociselny = 0;
                 double podil_realny = 0;
                 while (!ctenar.EndOfStream)
                 {
                     int cislo = int.Parse(ctenar.ReadLine());
-                    pole[poradi - 1] = cislo;            
+                    pole[poradi - 1] = cislo;
                     if (poradi == 5)
                     {
                         mocnina = cislo;
                         podil_realny = ((double)cislo / (double)n);
                         podil_celociselny = (cislo / n);
-                        for(int i = 1; i < n; i++)
+                        if (n == 0)
                         {
-                            mocnina *= cislo;
+                            mocnina = 1;
+                        }
+                        else
+                        {
+                            if (n > 0)
+                            {
+                                for (int i = 1; i < n; i++)
+                                {
+                                    mocnina *= cislo;
+                                }
+                            }
+                            if (n < 0)
+                            {
+                                n = Math.Abs(n);
+                                for (int i = 1; i < n; i++)
+                                {
+                                    mocnina *= cislo;
+                                }
+                                mocnina = 1 / mocnina;
+                            }
                         }
                     }
                     poradi++;
                 }
                 int soucet = 0;
-                foreach(int cislo in pole)
+                foreach (int cislo in pole)
                 {
                     soucet += cislo;
                 }
                 MessageBox.Show(pole[4] + " na " + n + ". = " + mocnina.ToString() + "\nCeločíselný podíl čísel " + pole[4] + " a " + n + ": " + podil_celociselny.ToString() + "\nReálný podíl čísel " + pole[4] + " a " + n + ": " + podil_realny.ToString() + "\nSoučet: " + soucet.ToString());
             }
-            catch(OverflowException ex)
+            catch (OverflowException ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -64,13 +84,21 @@ namespace WindowsFormsApp24242
             {
                 MessageBox.Show(ex.Message);
             }
-            catch(IndexOutOfRangeException ex)
+            catch (IndexOutOfRangeException ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            catch(FormatException ex)
+            catch (FormatException ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (ctenar != null)
+                {
+                    ctenar.Close();
+
+                }
             }
         }
     }
